ccf47fac39136f1e278208356e996a9636cffa91
brindharaja <rbrindha842@gmail.com>
2026-03-23 23:30:37 +0530

    Second commit

--- playwright.config.js
+++ playwright.config.js
@@ -3,6 +3,7 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
+  reporter: 'html',
   use: {
     trace: 'on-first-retry',
     viewport:null,
